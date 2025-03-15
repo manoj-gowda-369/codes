842a3dc7e8aa5b45a0389887079f40af2046981c
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-03-15 23:21:59 +0530

    lab 13 python program file

---
+++ 13lab.py
@@ -0,0 +1,13 @@
+name="manoj "
+
+if len(name) < 3:
+    
+ print("name must be 3 letters")
+
+elif   len(name) >  50:
+  print("name must be max 50 letters")
+
+else:
+  print("name look good!")
+
+       
