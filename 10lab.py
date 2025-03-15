5b530130d38b5e810c9f62cc96db2c981d223d8f
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-03-15 23:21:18 +0530

    lab 10 python program file

---
+++ 10lab.py
@@ -0,0 +1,9 @@
+house_price=1000000
+has_good_credit=False 
+if has_good_credit:
+    down_payment=0.1*house_price
+ 
+else:
+    down_payment=0.2*house_price
+
+print(f"down payment: {down_payment}")
