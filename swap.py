4e96f3b2b804b243be32d2f54a305d618f5af2e2
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-04-16 22:43:06 +0530

    updated

--- swap.py
+++ swap.py
@@ -1,1 +1,17 @@
+def fibonacci(n):
+    series = [0, 1]
+    for _ in range(n - 2):
+        series.append(series[-1] + series[-2])
+    return series[:n]
+
+num = int(input("Enter the number of terms: "))
+
+if num <= 0:
+    print("Please enter a positive integer.")
+else:
+    result = fibonacci(num)
+    print("Fibonacci Series:")
+    for i in result:
+        print(i, end=' ')
+    print("\nProgram by Manoj 🚀")
 
