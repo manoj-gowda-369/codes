be850f023b7a3a582de6c886e1382665f7d5f006
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-05-07 23:08:05 +0530

    stack using if else

--- stack.py
+++ stack.py
@@ -4,3 +4,9 @@ def push():
     item = input("Enter the item to push: ")
     stack.append(item)
     print(f"{item} has been pushed onto the stack.")
+    def pop():
+    if not stack:
+        print("Stack is empty. Nothing to pop.")
+    else:
+        item = stack.pop()
+        print(f"Popped item: {item}")
