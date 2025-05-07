4ec9e8189c82c60aa744f22daa9d6067e63fb311
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-05-07 23:20:58 +0530

    stack using if else

--- stack.py
+++ stack.py
@@ -6,14 +6,14 @@ def push():
     print(f"{item} has been pushed onto the stack.")
     def pop():
     if not stack:
-        print("Stack is empty. Nothing to pop.")
+        print("Stack is empty. Nothing is there to pop.")
     else:
         item = stack.pop()
         print(f"Popped item: {item}")
 
         def peek():
     if not stack:
-        print("Stack is empty. Nothing to peek.")
+        print("Stack is empty. Nothing is there to  peek.")
     else:
         print(f"Top of the stack: {stack[-1]}")
         def is_empty():
