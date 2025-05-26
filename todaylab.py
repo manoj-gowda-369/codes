1f48816df9ae55b0400f67e83f82fefbdf098b42
manoj-gowda-369 <manoj.g@s.amity.edu>
2025-05-26 23:44:17 +0530

    lab 25/6

--- todaylab.py
+++ todaylab.py
@@ -1,3 +1,8 @@
 print("ceil(4.3):", math.ceil(4.3))
 print("floor(4.7):", math.floor(4.7))
 print("trunc(4.9):", math.trunc(4.9))
+# === POWER & LOGARITHMS ===
+print("\n== POWER & LOGARITHMS ==")
+print("pow(2, 3):", math.pow(2, 3))
+print("sqrt(16):", math.sqrt(16))
+print("exp(2):", math.exp(2))
